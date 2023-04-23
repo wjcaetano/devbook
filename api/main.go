146dc9f2018253f3e5671086c6b1328db386edf6
Willian de Jesus Caetano <willian.caetano@mercadolivre.com>
2023-04-23 18:58:36 -0300

    Conectando com banco de dados

--- api/main.go
+++ api/main.go
@@ -10,10 +10,8 @@ import (
 
 func main() {
 	config.Carregar()
-
-	fmt.Println(config.Porta)
-	fmt.Println("Running API!")
 	r := router.Gerar()
 
+	fmt.Printf("Escutando na porta %d", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
